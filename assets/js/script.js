b00e0b6c9664b814ff7b8d12fe9a38c6a7ab8f02
Tydomx <michaelthai16@gmail.com>
2022-07-21 14:57:49 -0500

    Added hide class to make sure items aren't showing when not needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,48 @@
 let searchCityForm = document.querySelector('#search-city');
 let searchCityInput = document.querySelector('#search-input');
+let noDataMessage = document.querySelector('#no-data-message');
+let currentWeatherBox = document.querySelector('#current-weather-box');
+let forecastSection = document.querySelector('#forecast-section');
+let weatherBody = document.querySelector('#weather-body');
+let fiveDay = document.querySelector('#five-day');
+
+let apiKey = '425d9b6035ae956db1531e2fea0981f6';
+let apiUrl = 'https://api.openweathermap.org/data/3.0/';
 
 let showCitySearch = function () {
 	console.log('showCitySearch fn');
 };
 
-let weatherSearchHandler = function () {
-	console.log('weatherSearchHandler fn: ' + searchCity);
+let searchCurrentWeather = function (searchCity) {
+	console.log('searchCurrentWeather fn: ' + searchCity);
+
+	let currentWeatherUrl = apiUrl + 'weather?q=' + searchCity + '&units=imperial&PPID=' + apiKey;
+	console.log(currentWeatherUrl);
 };
 
-let addCityToSearch = function () {
+let weatherSearchHandler = function (searchCity) {
+	console.log('weatherSearchHandler fn: ' + searchCity);
+
+	// hide no data message
+	noDataMessage.classList.add('hide');
+
+	// hide current weather box until time to display
+	currentWeatherBox.classList.add('hide');
+
+	// hide forecast section from until time to display
+	forecastSection.classList.add('hide');
+
+	// clear data from current weather body area
+	weatherBody.textContent = '';
+
+	// clear data from five day forecast section
+	fiveDay.textContent = '';
+
+	// call search function for fetch data
+	searchCurrentWeather(searchCity);
+};
+
+let addCityToSearch = function (searchCity) {
 	console.log('addCityToSearch fn: ' + searchCity);
 };
 
